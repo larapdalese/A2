d8465a0e63b0a0cfd5317b1af180fa533301f676
Lara <126460907+larapdalese@users.noreply.github.com>
2024-10-28 12:41:10 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 df = pd.DataFrame(
     [
-       {"Nome da despesa": "Sephora", "Valor": 750.99, "Categoria": beleza},
-       {"Nome da despesa": "Starbucks", "Valor": 36, "Categoria": comida},
-       {"Nome da despesa": "Uber", "Valor": 15, "Categoria": transporte},
-       {"Nome da despesa": "Saraiva", "Valor": 45.50, "Categoria": livros}
+       {"Nome da despesa": "Sephora", "Valor": 750.99, "Categoria": "beleza"},
+       {"Nome da despesa": "Starbucks", "Valor": 36, "Categoria": "comida"},
+       {"Nome da despesa": "Uber", "Valor": 15, "Categoria": "transporte"},
+       {"Nome da despesa": "Saraiva", "Valor": 45.50, "Categoria": "livros"}
    ]
 )
 edited_df = st.data_editor(df, num_rows="dynamic")
