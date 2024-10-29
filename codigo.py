c7ff64500e4f628573dc42b9c5e56c8b35680963
Lara <126460907+larapdalese@users.noreply.github.com>
2024-10-29 11:36:02 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-
 df = pd.DataFrame(
     [
        {"Nome da despesa": "Sephora", "Valor": 750.99, "Categoria": "beleza", "Data": "2024-01-15"},
@@ -10,8 +9,12 @@ df = pd.DataFrame(
     ]
 )
 df["Data"] = pd.to_datetime(df["Data"]).dt.date
-meses_disponiveis = pd.to_datetime(df["Data"]).dt.strftime("%Y-%m").unique()
-mes_selecionado = st.selectbox("Selecione o mês (AAAA-MM):", meses_disponiveis)
-despesas_filtradas = df[pd.to_datetime(df["Data"]).dt.strftime("%Y-%m") == mes_selecionado]
+meses_disponiveis = ["Tudo"] + list(pd.to_datetime(df["Data"]).dt.strftime("%Y-%m").unique())
+mes_selecionado = st.selectbox("Selecione o mês (AAAA-MM) ou 'Tudo' para ver todos:", meses_disponiveis)
+if mes_selecionado == "Tudo":
+    despesas_filtradas = df
+else:
+    despesas_filtradas = df[pd.to_datetime(df["Data"]).dt.strftime("%Y-%m") == mes_selecionado]
 st.write("Despesas do mês selecionado:")
 st.write(despesas_filtradas)
+
