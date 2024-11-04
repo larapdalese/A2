d41575de9c563ed8c960648229af9c88210eddf9
Lara <126460907+larapdalese@users.noreply.github.com>
2024-11-04 12:15:48 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from datetime import datetime
-import pytz
-import time
 
-st.set_page_config(layout="wide")  
+# Aplicar estilo CSS para usar tela cheia
+st.set_page_config(layout="wide")  # Configurar a página para o layout expandido
+
+# Estilo CSS para reduzir margens laterais e expandir área de exibição
 st.markdown("""
     <style>
     /* Expandir o container principal */
@@ -22,9 +22,8 @@ st.markdown("""
     }
     </style>
     """, unsafe_allow_html=True)
-fuso_horario_brasilia = pytz.timezone("America/Sao_Paulo")
-data_hora_atual = datetime.now(fuso_horario_brasilia).strftime("%d/%m/%Y %H:%M:%S")
-st.write(f"**Data e Hora Atual (Brasília):** {data_hora_atual}")
+
+# Criar DataFrame de despesas
 df = pd.DataFrame(
     [
         {"Nome da despesa": "Sephora", "Data": "2024-01-15", "Categoria": "beleza", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 750.99},
@@ -63,96 +62,60 @@ df = pd.DataFrame(
         {"Nome da despesa": "Material escolar", "Data": "2024-07-15", "Categoria": "educação", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 150}, 
         {"Nome da despesa": "Show", "Data": "2024-07-20", "Categoria": "lazer", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 250},
         {"Nome da despesa": "Cinema", "Data": "2024-08-05", "Categoria": "lazer", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 50},
-        {"Nome da despesa": "Férias", "Data": "2024-08-20", "Categoria": "lazer", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 3000},
-        {"Nome da despesa": "Corte de cabelo", "Data": "2024-01-25", "Categoria": "beleza", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 150},
-        {"Nome da despesa": "Visita ao dentista", "Data": "2024-01-30", "Categoria": "saúde", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 400},
-        {"Nome da despesa": "Lanche na escola", "Data": "2024-02-10", "Categoria": "comida", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 25},
-        {"Nome da despesa": "Pizza", "Data": "2024-02-22", "Categoria": "comida", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 60},
-        {"Nome da despesa": "Passagem de ônibus", "Data": "2024-03-05", "Categoria": "transporte", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 10},
-        {"Nome da despesa": "Pedágio", "Data": "2024-03-20", "Categoria": "transporte", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 5},
-        {"Nome da despesa": "Maquiagem", "Data": "2024-04-10", "Categoria": "beleza", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 200},
-        {"Nome da despesa": "Remédios", "Data": "2024-04-25", "Categoria": "saúde", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 90},
-        {"Nome da despesa": "Camiseta", "Data": "2024-05-15", "Categoria": "vestuário", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 100},
-        {"Nome da despesa": "Calça", "Data": "2024-05-30", "Categoria": "vestuário", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 150},
-        {"Nome da despesa": "Lanche na padaria", "Data": "2024-06-10", "Categoria": "comida", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 30},
-        {"Nome da despesa": "Almoço com amigos", "Data": "2024-06-20", "Categoria": "comida", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 70},
-        {"Nome da despesa": "Passeio", "Data": "2024-07-10", "Categoria": "lazer", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 120},
-        {"Nome da despesa": "Jantar fora", "Data": "2024-07-25", "Categoria": "lazer", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 90},
-        {"Nome da despesa": "Manicure", "Data": "2024-08-15", "Categoria": "beleza", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 80},
-        {"Nome da despesa": "Exame médico", "Data": "2024-08-30", "Categoria": "saúde", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 200},
-        {"Nome da despesa": "Café", "Data": "2024-09-15", "Categoria": "comida", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 20},
-        {"Nome da despesa": "Sorvete", "Data": "2024-09-25", "Categoria": "comida", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 15},
-        {"Nome da despesa": "Passeio de bicicleta", "Data": "2024-10-20", "Categoria": "lazer", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 40},
-        {"Nome da despesa": "Show de música", "Data": "2024-10-30", "Categoria": "lazer", "Forma de pagamento": "crédito", "Tipo": "gasto", "Valor": 100}
+        {"Nome da despesa": "Férias", "Data": "2024-08-20", "Categoria": "lazer", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 3000}
     ]
 )
 df["Data"] = pd.to_datetime(df["Data"])
+
+# Título e entrada para orçamento
 st.markdown("<h1 style='text-align: center;'>Orçamento do mês:</h1>", unsafe_allow_html=True)
 orcamento = st.number_input("Insira o orçamento do mês:", min_value=0.0, format="%.2f")
 st.markdown("<h2 style='text-align: center;'>Opções</h2>", unsafe_allow_html=True)
+
+# Dividir layout em duas colunas de tamanhos iguais
 col1, col2 = st.columns(2)
+
+# Exibir gráfico de rosca à esquerda
 with col1:
     fig_pie = px.pie(df, names="Categoria", values="Valor", title="Distribuição de Gastos por Categoria", hole=0.4)
     fig_pie.update_layout(margin=dict(t=30, l=0, r=0, b=0))
     st.plotly_chart(fig_pie, use_container_width=True)
+
+# Exibir menu de opções à direita
 with col2:
-    escolha = st.radio("Opções", ["Todas as Despesas", "Gastos ao longo do tempo", "Mais 2"])
-    
+    escolha = st.radio("Opções", ["Todas as Despesas", "Por Categoria", "Mais 3"])
+
+    # Exibir DataFrames de acordo com a opção escolhida
     if escolha == "Todas as Despesas":
         st.write(df)
-    
-    elif escolha == "Mais 2":
-        escolha_mais_2 = st.radio("Opções adicionais", ["Por mês", "Adicionar despesa"])
+    elif escolha == "Por Categoria":
+        categoria_selecionada = st.selectbox("Escolha uma categoria:", df["Categoria"].unique())
+        despesas_categoria = df[df["Categoria"] == categoria_selecionada]
+        st.write(despesas_categoria)
+    elif escolha == "Mais 3":
+        escolha_mais_3 = st.radio("Opções adicionais", ["Por mês", "Gastos totais ao longo do tempo", "Gastos ao longo dos meses"])
         
-        if escolha_mais_2 == "Por mês":
+        if escolha_mais_3 == "Por mês":
             mes_selecionado = st.selectbox("Selecione o mês:", ["Tudo"] + list(df["Data"].dt.to_period("M").astype(str).unique()))
-            
             if mes_selecionado == "Tudo":
                 st.write(df)
             else:
                 despesas_mes = df[df["Data"].dt.to_period("M").astype(str) == mes_selecionado]
                 st.write(despesas_mes)
-        
-    elif escolha_mais_2 == "Gastos ao longo do tempo":
-        df["Data"] = pd.to_datetime(df["Data"], errors='coerce')  # Converte a coluna 'Data'
-
-        if df.empty:
-            st.warning("Não há dados disponíveis para exibir.")
-                     else:
-                         if df["Valor"].isnull().any():
-            st.warning("Existem valores nulos na coluna 'Valor'.")
-        else:
-            df["AnoMes"] = df["Data"].dt.to_period("M").astype(str)  # Cria a coluna AnoMes
-            despesas_por_mes = df.groupby("AnoMes")["Valor"].sum().reset_index()  # Agrupa por mês
-            
-            # Mostra os dados agrupados para depuração
-            st.write("Dados agrupados por mês:", despesas_por_mes)
-            
-            if despesas_por_mes.empty:
-                st.warning("Não há despesas para exibir.")
-            else:
-                fig_gastos_totais = px.line(despesas_por_mes, x="AnoMes", y="Valor", title="Gastos ao longo do tempo")
-                st.plotly_chart(fig_gastos_totais, use_container_width=True)
-
-
-        elif escolha_mais_2 == "Adicionar despesa":
-            with st.form(key='my_form'):
-                nome_despesa = st.text_input("Nome da despesa:")
-                data_despesa = st.date_input("Data da despesa:")
-                categoria_despesa = st.selectbox("Categoria:", df["Categoria"].unique())
-                forma_pagamento = st.selectbox("Forma de pagamento:", ["crédito", "débito", "pix"])
-                valor_despesa = st.number_input("Valor da despesa:", min_value=0.0, format="%.2f")
-                submit_button = st.form_submit_button(label='Adicionar despesa')        
                 
-                if submit_button:
-                    nova_despesa = {
-                        "Nome da despesa": nome_despesa,
-                        "Data": data_despesa,
-                        "Categoria": categoria_despesa,
-                        "Forma de pagamento": forma_pagamento,
-                        "Tipo": "gasto",
-                        "Valor": valor_despesa
-                    }
-                    df = df.append(nova_despesa, ignore_index=True)  
-                    st.success("Despesa adicionada com sucesso!")
-                    st.write(df)
+        elif escolha_mais_3 == "Gastos totais ao longo do tempo":
+            df["AnoMes"] = df["Data"].dt.to_period("M").astype(str)
+            despesas_por_mes = df.groupby("AnoMes")["Valor"].sum().reset_index()
+            fig_gastos_totais = px.line(despesas_por_mes, x="AnoMes", y="Valor", title="Gastos Totais ao Longo do Tempo")
+            st.plotly_chart(fig_gastos_totais, use_container_width=True)
+        
+        elif escolha_mais_3 == "Gastos ao longo dos meses":
+            df["Mes"] = df["Data"].dt.month
+            despesas_por_mes = df.groupby("Mes")["Valor"].sum().reset_index()
+            fig_gastos_mensais = px.line(despesas_por_mes, x="Mes", y="Valor", title="Gastos ao Longo dos Meses")
+            st.plotly_chart(fig_gastos_mensais, use_container_width=True)
+
+ax.set_title('Despesas por Categoria')
+
+# Exibindo o gráfico no Streamlit
+st.pyplot(fig)
