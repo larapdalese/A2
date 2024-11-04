36cce390c278ffa353b5ff9c9aa97c9c5eca9567
Lara <126460907+larapdalese@users.noreply.github.com>
2024-11-04 11:35:11 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 
-st.set_page_config(layout="wide")  # Configurar a página para o layout expandido
-
-# Estilo CSS para reduzir margens laterais e expandir área de exibição
+st.set_page_config(layout="wide")  
 st.markdown("""
     <style>
     /* Expandir o container principal */
@@ -21,70 +20,7 @@ st.markdown("""
     }
     </style>
     """, unsafe_allow_html=True)
-
-#### Alinne
-
-pages = {
-    "Your account": [
-        st.Page("create_account.py", title="Create your account"),
-        st.Page("manage_account.py", title="Manage your account"),
-    ],
-    "Resources": [
-        st.Page("learn.py", title="Learn about us"),
-        st.Page("trial.py", title="Try it out"),
-    ],
-}
-
-pg = st.navigation(pages)
-pg.run()
-
-st.set_page_config(layout="wide")
-
-# Estilo e posicionamento dos elementos
-st.markdown("""
-    <style>
-        .container {
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-            margin-left: 10px;
-            width: 200px;
-        }
-        .calendar, .clock {
-            color: #FFFFFF;
-            background-color: #4D5D39;
-            border-radius: 5px;
-            padding: 10px;
-            text-align: center;
-            width: 100%;
-            margin-bottom: 10px;
-        }
-        .clock {
-            background-color: #A2B09F;
-            font-size: 24px;
-        }
-    </style>
-    <div class="container">
-        <div class="calendar">
-            <h3>{}</h3>
-        </div>
-        <div class="clock" id="clock"></div>
-    </div>
-
-    <script>
-        function updateTime() {
-            var now = new Date();
-            var timeString = now.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: true});
-            document.getElementById('clock').innerHTML = timeString;
-        }
-        setInterval(updateTime, 1000);
-        updateTime();
-    </script>
-""".format(datetime.now().strftime("%A, %B %d, %Y")), unsafe_allow_html=True)
-
-####
-
-# Criar DataFrame de despesas
+st.markdown(f"<h3 style='text-align: right;'>{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}</h3>", unsafe_allow_html=True)
 df = pd.DataFrame(
     [
         {"Nome da despesa": "Sephora", "Data": "2024-01-15", "Categoria": "beleza", "Forma de pagamento": "débito", "Tipo": "gasto", "Valor": 750.99},
@@ -147,56 +83,50 @@ df = pd.DataFrame(
     ]
 )
 df["Data"] = pd.to_datetime(df["Data"])
-
-
-# Título e entrada para orçamento
 st.markdown("<h1 style='text-align: center;'>Orçamento do mês:</h1>", unsafe_allow_html=True)
 orcamento = st.number_input("Insira o orçamento do mês:", min_value=0.0, format="%.2f")
 st.markdown("<h2 style='text-align: center;'>Opções</h2>", unsafe_allow_html=True)
-
-# Dividir layout em duas colunas de tamanhos iguais
 col1, col2 = st.columns(2)
-
-# Exibir gráfico de rosca à esquerda
 with col1:
     fig_pie = px.pie(df, names="Categoria", values="Valor", title="Distribuição de Gastos por Categoria", hole=0.4)
     fig_pie.update_layout(margin=dict(t=30, l=0, r=0, b=0))
     st.plotly_chart(fig_pie, use_container_width=True)
-
-# Exibir menu de opções à direita
 with col2:
-    escolha = st.radio("Opções", ["Todas as Despesas", "Gastos ao Longo do Tempo", "Adicionar Despesa"])
-
-    # Exibir DataFrames de acordo com a opção escolhida
+    escolha = st.radio("Opções", ["Todas as Despesas", "Por Categoria", "Mais 4"])
     if escolha == "Todas as Despesas":
         st.write(df)
-    elif escolha == "Mais 3":
-        escolha_mais_3 = st.radio("Opções adicionais", ["Por mês", "Gastos ao Longo do Tempo", "Adicionar Despesa"])
-        
-        if escolha_mais_3 == "Por mês":
+    elif escolha == "Por Categoria":
+        categoria_selecionada = st.selectbox("Escolha uma categoria:", df["Categoria"].unique())
+        despesas_categoria = df[df["Categoria"] == categoria_selecionada]
+        st.write(despesas_categoria)
+    elif escolha == "Mais 4":
+        escolha_mais_4 = st.radio("Opções adicionais", ["Por mês", "Gastos totais ao longo do tempo", "Categorias ao longo dos meses", "Adicionar despesa"])
+        if escolha_mais_4 == "Por mês":
             mes_selecionado = st.selectbox("Selecione o mês:", ["Tudo"] + list(df["Data"].dt.to_period("M").astype(str).unique()))
             if mes_selecionado == "Tudo":
                 st.write(df)
             else:
                 despesas_mes = df[df["Data"].dt.to_period("M").astype(str) == mes_selecionado]
                 st.write(despesas_mes)
-                
-        elif escolha_mais_3 == "Gastos ao Longo do Tempo":
+        elif escolha_mais_4 == "Gastos totais ao longo do tempo":
             df["AnoMes"] = df["Data"].dt.to_period("M").astype(str)
             despesas_por_mes = df.groupby("AnoMes")["Valor"].sum().reset_index()
-            fig_gastos_totais = px.line(despesas_por_mes, x="AnoMes", y="Valor", title="Gastos ao Longo do Tempo")
+            fig_gastos_totais = px.line(despesas_por_mes, x="AnoMes", y="Valor", title="Gastos Totais ao Longo do Tempo")
             st.plotly_chart(fig_gastos_totais, use_container_width=True)
-        
-        elif escolha_mais_3 == "Adicionar despesa":
+        elif escolha_mais_4 == "Categorias ao longo dos meses":
+            df["Mes"] = df["Data"].dt.month
+            despesas_por_categoria_mes = df.groupby(["Mes", "Categoria"])["Valor"].sum().reset_index()
+            fig_categorias_mensais = px.bar(despesas_por_categoria_mes, x="Mes", y="Valor", color="Categoria", 
+                                             title="Categorias ao Longo dos Meses", barmode="group")
+            st.plotly_chart(fig_categorias_mensais, use_container_width=True)
+        elif escolha_mais_4 == "Adicionar despesa":
             with st.form(key='my_form'):
                 nome_despesa = st.text_input("Nome da despesa:")
                 data_despesa = st.date_input("Data da despesa:")
                 categoria_despesa = st.selectbox("Categoria:", df["Categoria"].unique())
                 forma_pagamento = st.selectbox("Forma de pagamento:", ["crédito", "débito", "pix"])
                 valor_despesa = st.number_input("Valor da despesa:", min_value=0.0, format="%.2f")
-                
-                submit_button = st.form_submit_button(label='Adicionar despesa')
-                
+                submit_button = st.form_submit_button(label='Adicionar despesa')        
                 if submit_button:
                     nova_despesa = {
                         "Nome da despesa": nome_despesa,
@@ -206,6 +136,6 @@ with col2:
                         "Tipo": "gasto",
                         "Valor": valor_despesa
                     }
-                    df = df.append(nova_despesa, ignore_index=True)  # Adiciona a nova despesa ao DataFrame
+                    df = df.append(nova_despesa, ignore_index=True)  
                     st.success("Despesa adicionada com sucesso!")
-                    st.write(df)  # Exibe o DataFrame atualizado
+                    st.write(df)  
