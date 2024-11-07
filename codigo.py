9ce351f43997dbbf36aac1d54d03e6b7b5127e1e
citlaline <168496446+citlaline@users.noreply.github.com>
2024-11-07 13:29:48 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -3,6 +3,14 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+### Alinne
+st.page_link("https://financedivas.streamlit.app", label="Início", icon="🏠")
+st.page_link("pages/page_1.py", label="Gráficos")
+st.page_link("pages/page_2.py", label="Insights de Gastos", disabled=True)
+st.page_link("http://www.google.com", label="Notícias", icon="🌎")
+
+###
+
 st.set_page_config(layout="wide")
 def apply_custom_css():
     st.markdown("""
