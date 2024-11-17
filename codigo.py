8e4699ee0a93a553df6d2fd2bd068c3cd0daa072
manubmuniz <manubmuniz@gmail.com>
2024-11-16 23:08:54 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+st.set_page_config()
 st.set_page_config(layout="wide", page_title="Meu Site de Finanças")
 def apply_custom_css():
     st.markdown("""
