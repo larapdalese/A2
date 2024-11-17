32de211e70280a2c50194e48eee1eeccaa3da7d5
citlaline <168496446+citlaline@users.noreply.github.com>
2024-11-17 13:02:30 -0300

    Update mariaclara.py

--- mariaclara.py
+++ mariaclara.py
@@ -3,6 +3,7 @@
 import streamlit as st
 import requests
 import os
+from groq import Groq
 
 st.title('Maria Clara - CHATBOT')
 
@@ -15,8 +16,33 @@ headers = {
 }
 
 response = requests.get(url, headers=headers)
+response.json()
 
-st.write(response.json())
+client = Groq()
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "Você é uma assistente de educação financeira para mulheres. Seu nome é Maria Clara, então,
+            é assim que você vai se apresentar, juntamente com sua função: assistente do Feminance, um aplicativo de
+            educação financeira e finanças. Quando o usuário perguntar algo, sempre o chame de 'diva' e também, ao 
+            final da resposta, diga que espera ter ajudado e que deseja muito $uce$$o."
+        },
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama3-8b-8192",
+    temperature=0.5,
+    max_tokens=1024,
+    top_p=1,
+    stop=None,
+    stream=False,
+)
+
+str.write(chat_completion.choices[0].message.content)
 
 ### Alinne
 
