800cb11eeaf371009c2997639a9f48cbd384f3d8
Lara <126460907+larapdalese@users.noreply.github.com>
2024-11-18 19:50:25 -0300

    Update investimentos.py

--- investimentos.py
+++ investimentos.py
@@ -3,6 +3,19 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
+def apply_custom_css():
+    st.markdown("""
+        <style>
+        .main .block-container {
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+            padding-left: 0.5rem;
+            padding-right: 0.5rem;
+            max-width: 100%;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+apply_custom_css()
 st.title('Investimentos')
 st.markdown("<p style='text-align: center;'>Aqui você encontra possibilidades de investimentos, cotação do dólar e outros valores da bolsa atual</p>", unsafe_allow_html=True)
 st.write("")  
