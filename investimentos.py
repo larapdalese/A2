26e5f12515bd7e43c9ecb6ff824307661dde841d
Lara <126460907+larapdalese@users.noreply.github.com>
2024-11-18 19:57:41 -0300

    Update investimentos.py

--- investimentos.py
+++ investimentos.py
@@ -2,6 +2,20 @@
 #aqui o ususário encontra possibilidades de investimentos e, ao final da página, uma raspagem da bolsa de valores
 import streamlit as st
 import yfinance as yf
+st.set_page_config(layout="wide", page_title="Investimentos")
+def apply_custom_css():
+    st.markdown("""
+        <style>
+        .main .block-container {
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+            padding-left: 0.5rem;
+            padding-right: 0.5rem;
+            max-width: 100%;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+apply_custom_css()
 st.markdown("""
     <style>
     .centered-title {
@@ -15,7 +29,7 @@ st.markdown("""
     }
     </style>
     <h1 class="centered-title">Investimentos</h1>
-    <p class="centered-subtitle">Aqui você encontra possibilidades de investimentos, cotação do dólar e outros valores da bolsa atual. Caso não entenda algo, a Maria Clara estará sempre à sua disposição<3</p>
+    <p class="centered-subtitle">Aqui você encontra possibilidades de investimentos, cotação do dólar e outros valores da bolsa atual. Caso não entenda algo, a Maria Clara estará sempre à sua disposição!<3</p>
     """, unsafe_allow_html=True)
 ticker = 'USDBRL=X'
 dados = yf.download(ticker, start='2023-01-01', end='2024-11-18')
