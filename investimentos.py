9e3f5bbe482fffbb9a23510eeebd8099b4feaab7
Lara <126460907+larapdalese@users.noreply.github.com>
2024-11-18 22:03:12 -0300

    Update investimentos.py

--- investimentos.py
+++ investimentos.py
@@ -43,11 +43,18 @@ with col1:
 with col2:
     st.subheader("Gráficos de Cotação")
     def exibir_grafico_cotacao(ticker, moeda):
-        today = datetime.datetime.today().strftime('%Y-%m-%d')  
-        dados = yf.download(ticker, start='2023-01-01', end=today)
+        today = datetime.datetime.today().strftime('%Y-%m-%d') 
+        dados = yf.download(ticker, start='2023-01-01', end=today)      
         if not dados.empty:
             st.markdown(f"**{moeda}**")
             st.line_chart(dados['Close'])
+            cotacao_dia = dados['Close'][-1]
+            st.write(f"Cotação do dia: R$ {cotacao_dia:.2f}")
+            if len(dados) >= 30:
+                media_30_dias = dados['Close'][-30:].mean()
+                st.write(f"Média dos últimos 30 dias: R$ {media_30_dias:.2f}")
+            else:
+                st.write("Média dos últimos 30 dias: Não disponível (menos de 30 dias de dados)")
         else:
             st.error(f'Não foi possível obter os dados da cotação do {moeda}.')
     exibir_grafico_cotacao('USDBRL=X', 'Dólar')
