a206b8e06c7385c8a1c893e6cd7e21b671f6390c
manubmuniz <manubmuniz@gmail.com>
2024-11-20 21:06:48 -0300

    Update dicas.py

--- dicas.py
+++ dicas.py
@@ -1,11 +1,8 @@
-import streamlit as st
-import requests
-
 # Função para buscar notícias sobre educação financeira usando a GNews API
-def buscar_noticias_gnews(api_key, termos_busca, pagina=1):
+def buscar_noticias_gnews(api_key, termos_busca):
     try:
         # Definindo a URL da API e os parâmetros
-        api_url = f"https://gnews.io/api/v4/search?q={termos_busca}&lang=pt&token={api_key}&country=br&page={pagina}"
+        api_url = f"https://gnews.io/api/v4/search?q={termos_busca}&lang=pt&token={api_key}&country=br"
         
         # Fazendo a requisição GET à GNews API
         response = requests.get(api_url)
@@ -25,41 +22,22 @@ def mostrar_dicas():
 
     # Parâmetros da API (substitua pelo valor real)
     api_key = "d700b8cb09b888dc838bf50109bedd9e"  # Substitua pela sua chave API válida
-    termos_busca = st.text_area("Busque por palavras-chave para encontrar resultados ainda melhores, diva:", "", height=100)
+    termos_busca = "educação financeira para mulheres OR finanças pessoais femininas"
 
     # Buscar notícias usando a API
-    pagina = st.number_input("Página", min_value=1, step=1, value=1)
-    noticias = buscar_noticias_gnews(api_key, termos_busca, pagina)
+    noticias = buscar_noticias_gnews(api_key, termos_busca)
 
     # Se não houver notícias encontradas
     if not noticias:
         st.write("Nenhuma notícia encontrada. Tente novamente mais tarde.")
         return
 
-    # Mostrar as notícias encontradas (4 por página)
-    noticias_por_pagina = 4
-    inicio = (pagina - 1) * noticias_por_pagina
-    fim = inicio + noticias_por_pagina
-    noticias_pagina = noticias[inicio:fim]
-
-    for noticia in noticias_pagina:
+    # Mostrar as notícias encontradas
+    for noticia in noticias:
         st.subheader(noticia['title'])
         st.write(noticia['description'])
         st.write(f"[Leia mais]({noticia['url']})")
         st.markdown("---")
-
-    # Rodapé com controle de páginas
-    st.markdown("---")
-    total_paginas = len(noticias) // noticias_por_pagina + (1 if len(noticias) % noticias_por_pagina > 0 else 0)
-    col1, col2, col3 = st.columns([1, 2, 1])
-    with col2:
-        st.write(f"Página {pagina} de {total_paginas}")
-        if pagina < total_paginas:
-            if st.button("Próxima Página"):
-                st.experimental_rerun()
-        if pagina > 1:
-            if st.button("Página Anterior"):
-                st.experimental_rerun()
 
 # Rodar a função de dicas se for o arquivo principal
 if __name__ == "__main__":
