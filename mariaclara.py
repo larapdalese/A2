5a2b7da613aed9a8bee40420c332dcd10ddc0a57
citlaline <168496446+citlaline@users.noreply.github.com>
2024-11-22 12:51:26 -0300

    Update mariaclara.py

--- mariaclara.py
+++ mariaclara.py
@@ -1,8 +1,8 @@
 ### Alinne
 
 import streamlit as st
-import os
 from groq import Groq
+from spellchecker import SpellChecker
 
 st.set_page_config(page_title="Maria Clara - CHATBOT")
 
@@ -10,6 +10,12 @@ if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Oi, meu nome é Maria Clara! Como posso te ajudar hoje?"}]
 
 api_key = "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"
+
+def corrigir_ortografia(texto):
+    spell = SpellChecker(language='pt')
+    palavras = texto.split()
+    corrigido = [spell.correction(palavra) for palavra in palavras]
+    return ' '.join(corrigido)
 
 def generate_groq_response(prompt_input):
     client = Groq(api_key=api_key)
@@ -23,14 +29,14 @@ def generate_groq_response(prompt_input):
             "ao final da resposta, diga que espera ter ajudado e que deseja muito $uce$$o."
         )}
     ]
-
-
+    
     for dict_message in st.session_state.messages:
         messages.append(dict_message)
 
     messages.append({"role": "user", "content": prompt_input})
 
     try:
+        # Chamar a API Groq para gerar uma resposta
         chat_completion = client.chat.completions.create(
             messages=messages,
             model="llama3-groq-8b-8192-tool-use-preview",
@@ -41,7 +47,10 @@ def generate_groq_response(prompt_input):
             stream=None,
         )
 
-        return chat_completion.choices[0].message.content
+        resposta = chat_completion.choices[0].message.content
+
+        resposta_corrigida = corrigir_ortografia(resposta)
+        return resposta_corrigida
 
     except Exception as e:
         return f"Erro ao chamar a API Groq: {str(e)}"
@@ -52,17 +61,18 @@ for message in st.session_state.messages:
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Como posso te ajudar hoje, diva?"}]
+
 st.sidebar.button('Limpar histórico de chat', on_click=clear_chat_history)
 
 if prompt := st.chat_input("Digite sua pergunta:"):
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt) 
 
+    with st.chat_message("user"):
+        st.write(prompt)
     with st.chat_message("assistant"):
-        with st.spinner("Pensando..."): 
+        with st.spinner("Pensando..."):
             response = generate_groq_response(prompt)
-            st.write(response) 
+            st.write(response)
 
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
