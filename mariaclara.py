4bed78c6b41af2f1017c2b21c4761137105d84ef
citlaline <168496446+citlaline@users.noreply.github.com>
2024-11-22 11:49:53 -0300

    Update mariaclara.py

--- mariaclara.py
+++ mariaclara.py
@@ -32,7 +32,7 @@ def generate_groq_response(prompt_input):
         chat_completion = client.chat.completions.create(
             messages=messages,
             model="llama3-8b-8192",
-            temperature=0.5,
+            temperature=0.2,
             max_tokens=1024,
             top_p=1,
             stop=None,
