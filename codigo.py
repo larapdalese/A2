870ab388f953aa925afa92e92b3c53e89fe99bea
citlaline <168496446+citlaline@users.noreply.github.com>
2024-11-24 19:55:54 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -596,10 +596,6 @@ def investimentos_page():
         exibir_grafico_cotacao('EURBRL=X', 'Euro')
 
 
-def add_image_to_sidebar():
-    image_path = "logo.png"
-    st.sidebar.image(image_path, caption="Logo do site", use_column_width=True)
-
 pages = {
     "Essencial": [
         st.Page(home_page, title="Home", icon=None, url_path=None, default=False),
@@ -612,8 +608,6 @@ pages = {
        st.Page(investimentos_page, title="Invista como uma garota", icon=None, url_path=None, default=False),
     ],
 }
-
-add_image_to_sidebar()
 
 pg = st.navigation(pages, position="sidebar", expanded=False)
 pg.run()
