42471f011379909a5048373c5cad3fa698da6e50
citlaline <168496446+citlaline@users.noreply.github.com>
2024-11-24 19:24:37 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -8,6 +8,8 @@ import streamlit as st
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import yfinance as yf
+import datetime
 
 def home_page():
     st.set_page_config(page_title="Home")
@@ -511,7 +513,92 @@ def insights_page():
 
 
 def investimentos_page():
-    st.write("Página de Bolsa Atual. Coloque aqui as informações e funcionalidades da página de Bolsa Atual.")
+    image_path = "logo.png"
+    col1, col2, col3 = st.columns([1, 6, 1])
+    with col2:
+        st.image(image_path, caption=None, width=300, use_column_width=False, clamp=False, channels="RGB", output_format="auto")
+        
+    st.set_page_config(layout="wide", page_title="Investimentos")
+    def apply_custom_css():
+        st.markdown("""
+            <style>
+            .main .block-container {
+                padding-top: 1rem;
+                padding-bottom: 1rem;
+                padding-left: 0.5rem;
+                padding-right: 0.5rem;
+                max-width: 100%;
+            }
+            </style>
+        """, unsafe_allow_html=True)
+    apply_custom_css()
+    st.markdown("""
+        <style>
+        .centered-title {
+            text-align: center;
+            font-size: 36px;
+            font-weight: bold;
+        }
+        .centered-subtitle {
+            text-align: center;
+            font-size: 18px;
+        }
+        </style>
+        <h1 class="centered-title">Investimentos</h1>
+        <p class="centered-subtitle">
+            Aqui você encontra possibilidades de investimentos, cotação do dólar e outros valores da <br>
+            bolsa atual. Caso não entenda algo, a Maria Clara estará sempre à sua disposição!<3
+        </p>
+        """, unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Tipos de investimento")
+        st.markdown("Saiba um pouco mais sobre investimentos e as alternativas para o dinheiro que está sobrando na conta e que você não deseja gastar com mimos, clique nas palavras azuis sublinhadas para ser direcionada a páginas que te trarão mais informações sobre os tipos de investimentos.")
+        st.markdown("""
+        <style>
+        .custom-subsubtitle {
+            font-size: 20px; 
+            font-weight: bold; 
+            margin-bottom: 10px; 
+        }
+        </style>
+        <p class="custom-subsubtitle">Investimentos de baixo risco</p>
+    """, unsafe_allow_html=True)
+        st.markdown("Rendem menos & são mais seguros!")
+        st.markdown("""
+            - **Tesouro Direto**: 
+            [Um dos investimentos mais seguros do Brasil, ideal para quem busca estabilidade.](https://www.tesourodireto.com.br/)
+        """)
+        st.markdown("""
+            - **CDB**: 
+            [Certificados de Depósito Bancário que oferecem segurança e rendimento superior à poupança, cada banco possui o seu.](https://www.b3.com.br/pt_br/produtos-e-servicos/registro/renda-fixa-e-valores-mobiliarios/certificado-de-deposito-bancario.htm)
+        """)
+        st.markdown("""
+            - **Poupança**: 
+            [Apesar de menos rentável, ainda é uma opção segura para reservas de emergência.](https://www.gov.br/pt-br/servicos-estaduais/conta-poupanca)
+        """)
+        st.markdown("""
+        <style>
+        .custom-subsubtitle {
+            font-size: 20px; 
+            font-weight: bold; 
+            margin-bottom: 10px; 
+        }
+        </style>
+        <p class="custom-subsubtitle">Investimentos de médio risco</p>
+    """, unsafe_allow_html=True)
+    with col2:
+        st.subheader("Gráficos de Cotação")
+        def exibir_grafico_cotacao(ticker, moeda):
+            today = datetime.datetime.today().strftime('%Y-%m-%d')  # Data atual
+            dados = yf.download(ticker, start='2023-01-01', end=today)
+            if not dados.empty:
+                st.markdown(f"**{moeda}**")
+                st.line_chart(dados['Close'])
+            else:
+                st.error(f'Não foi possível obter os dados da cotação do {moeda}.')
+        exibir_grafico_cotacao('USDBRL=X', 'Dólar')
+        exibir_grafico_cotacao('EURBRL=X', 'Euro')
 
 pages = {
     "Essencial": [
